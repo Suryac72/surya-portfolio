d169419c2b145c872750938376796ce814a0598e
Surya Prakash Chaudhary <suryac72@gmail.com>
2021-11-29 00:28:09 +0530

    Commited after completing portfolio section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,13 @@
 import "./intro.scss"
+import {init} from "ityped";
+import {useEffect,useRef} from "react";
 export default function Intro() {
+    const textRef = useRef();
+    useEffect(()=>{
+      init(textRef.current, { showCursor: true, 
+        backdelay:1500,backSpeed:50,
+        strings: [' Developer', ' Designer'], });
+    },[]);
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -11,7 +19,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Surya Prakash Chaudhary</h1>
-                    <h3>Software Developer<span> Java Programmer</span></h3>
+                    <h3>Website<span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/down.png" alt="" />
