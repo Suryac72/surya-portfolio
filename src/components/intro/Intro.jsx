acc730f6f4aeae0aa40a44eff92fc3baa1dfb42b
Surya Prakash Chaudhary <suryac72@gmail.com>
2021-11-28 14:42:22 +0530

    Portfolio project commited before typed effect

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,22 @@
+import "./intro.scss"
+export default function Intro() {
+    return (
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Surya Prakash Chaudhary</h1>
+                    <h3>Software Developer<span> Java Programmer</span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
+        </div>
+    )
+}
