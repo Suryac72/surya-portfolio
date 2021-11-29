dbf597312de447d8aa4e85dddb006f8849aac85b
Surya Prakash Chaudhary <suryac72@gmail.com>
2021-11-29 22:30:07 +0530

    Commited after completion but some bugs  remaining

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,29 +1,82 @@
 import "./works.scss";
+import { useState } from "react";
 export default function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    {
+      id: "1",
+      icon: "./assets/mobile.png",
+      title: "Text Utility React App",
+      desc: `This project is based on React JS. In this web application, we are able
+        to perform simple text manipulation like change text to lowercase,
+        uppercase, copy text to clipboard, clear text, dark mode, and text to
+        speech functionality`,
+      img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    },
+    {
+      id: "2",
+      icon: "https://media2.gmgroup.be/00_nm_logo.png",
+      title: "News Monkey App",
+      desc: `News Monkey is API and React-based web application that provides
+        the latest news according to the date on which we visit the application`,
+      img: "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    },
+    {
+      id: "3",
+      icon: "./assets/writing.png",
+      title: "Branding",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
+
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+  
   return (
     <div className="works" id="works">
-      <div className="slider">
-        <div className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <img src="assets/mobile.png" alt="" />
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>Projects</span>
                 </div>
-                <h2>Title</h2>
-                <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga illum quos, eveniet sapiente reprehenderit sequi, dicta maiores
-                molestiae obcaecati quas, suscipit eos.
-              </p>
-              <span>Projects</span>
+              </div>
+              <div className="right">
+                <img
+                  src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                  alt=""
+                />
               </div>
             </div>
-            <div className="right">
-                <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930" alt="" />
-            </div>
           </div>
-        </div>
+        ))}
       </div>
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
     </div>
   );
 }
